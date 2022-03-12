6e0687bea9e999598132bd85a89ca0d5dcd47e92
Alyaa <alyaa.moh.bakr@gmail.com>
2022-03-12 15:52:23 +0200

    second commit

--- ALU.v
+++ ALU.v
@@ -1,14 +1,14 @@
 /**************************************************************************************************/
 /**************************************************************************************************/
-/*****************************		Author: Alyaa Mohamed  ************************************/
-/*****************************		Module: ALU	       ************************************/
+/*****************************		Author: Alyaa Mohamed      ************************************/
+/*****************************		Module: ALU	               ************************************/
 /**************************************************************************************************/
 /**************************************************************************************************/
 
 module ALU
 (
-	input  wire    [31:0]        SrcA									,
-	input  wire    [31:0]        SrcB									,
+	input  wire    [31:0]        SrcA										,
+	input  wire    [31:0]        SrcB										,
 	input  wire    [2:0]         ALUControl									,
 
 	output reg     [31:0]        ALUResult									,
@@ -26,11 +26,11 @@ module ALU
 			3'b000 : ALUResult = SrcA & SrcB							;
 			3'b001 : ALUResult = SrcA | SrcB							;
 			3'b010 : ALUResult = SrcA + SrcB							;
-			3'b011 : ALUResult = 32'b0								;
+			3'b011 : ALUResult = 32'b0									;
 			3'b100 : ALUResult = SrcA - SrcB							;
 			3'b101 : ALUResult = SrcA * SrcB							;
-			3'b110 : ALUResult = (SrcA < SrcB) ? 32'b1 : 32'b0 					;
-			3'b111 : ALUResult = 32'b0								;
+			3'b110 : ALUResult = (SrcA < SrcB) ? 32'b1 : 32'b0 			;
+			3'b111 : ALUResult = 32'b0									;
 		endcase
 
 	end
@@ -43,15 +43,15 @@ module ALU
 		
 			if ((ALUControl == 3'b011) || (ALUControl == 3'b111))
 			
-					Zero_flag = 1'b0							;
-			
-			else if (ALUResult == 32'b0)	
-				
-					Zero_flag = 1'b1							;
-   	
-			else 	
-				
-					Zero_flag = 1'b0							;
+					Zero_flag = 1'b0									;
+					
+			else if (ALUResult == 32'b0)			
+						
+					Zero_flag = 1'b1									;
+		
+			else 			
+						
+					Zero_flag = 1'b0									;
 		  
 		
 		end   
